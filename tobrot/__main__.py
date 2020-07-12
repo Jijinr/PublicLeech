45b6131bde80a90f227ef486204e928fc53cf622
judhistirpradhan <58895809+judhistirpradhan@users.noreply.github.com>
2020-07-12 21:51:08 +0530

    Update @MCS4U ytdlleech3bot

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -62,7 +62,7 @@ if __name__ == "__main__" :
     #
     # incoming_message_handler = MessageHandler(
     #     incoming_message_f,
-    #     filters=Filters.command(["leech"]) & Filters.chat(chats=AUTH_CHANNEL)
+    #     filters=Filters.command(["leech@ytdLeech3bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     # )
     # app.add_handler(incoming_message_handler)
     #
@@ -74,7 +74,7 @@ if __name__ == "__main__" :
     #
     # incoming_youtube_dl_handler = MessageHandler(
     #     incoming_youtube_dl_f,
-    #     filters=Filters.command(["ytdl"]) & Filters.chat(chats=AUTH_CHANNEL)
+    #     filters=Filters.command(["ytdl@ytdLeech3bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     # )
     # app.add_handler(incoming_youtube_dl_handler)
     #
@@ -133,13 +133,13 @@ if __name__ == "__main__" :
     #
     save_thumb_nail_handler = MessageHandler(
         save_thumb_nail,
-        filters=Filters.command(["savethumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["savethumbnail@ytdLeech3bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(save_thumb_nail_handler)
     #
     clear_thumb_nail_handler = MessageHandler(
         clear_thumb_nail,
-        filters=Filters.command(["clearthumbnail"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["clearthumbnail@ytdLeech3bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(clear_thumb_nail_handler)
     # 
